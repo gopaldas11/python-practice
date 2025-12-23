14a6e742b0ed21d3eb30430b5c0ffbcd11a43dfd
gopaldas11 <gopaldas535251@gmail.com>
2025-12-23 09:40:32 +0600

    Day 3 practice file

---
+++ Day_3-cls_10+/len_fun.py
@@ -0,0 +1,13 @@
+# This is an code of len function.
+name = input("Enter your name:")
+length = len(name)
+print(length)
+
+# replacing one variable data with another.
+a = input("a=")
+b = input("b=")
+c = a
+a = b
+b = c
+print("a=", a)
+print("b=", b)
