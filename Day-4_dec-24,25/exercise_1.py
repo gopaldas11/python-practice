03ccc955056e6f33e951b9fdba4a91370c2125ab
gopaldas11 <gopaldas535251@gmail.com>
2025-12-25 01:42:10 +0800

    1st half

---
+++ Day-4_dec-24,25/exercise_1.py
@@ -0,0 +1,5 @@
+#We have to make two digit input taking code that will do sum between them and show the sum.
+
+num_take = (input("Enter a two digit number: "))
+print("The sum of two numbers: "[0] + [1], num_take)
+#the rest of the data will be updated tomorrow
