2c9221ce1e8d250aa1af81e713cd97b1dc71a3b7
gopaldas11 <gopaldas535251@gmail.com>
2025-12-25 14:56:16 +0800

    Updated

---
+++ Day-4_dec-24,25/two_digit_sum.py
@@ -0,0 +1,7 @@
+#writing a code for an input of two digit number and makin sum of the two digits.
+
+two_digit_number = input("Enter a two digit number: ")
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+Digit_Sum = first_digit + second_digit
+print(Digit_Sum)
