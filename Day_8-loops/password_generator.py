9d08a7d36ed928f40a956eb908f8bafe4b23d009
gopaldas11 <gopaldas535251@gmail.com>
2025-12-31 19:20:34 +0600

    while loop

--- Day_8-loops/password_generator.py
+++ Day_8-loops/password_generator.py
@@ -4,7 +4,6 @@ latters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','
             'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 numbers = ['0','1','2','3','4','5','6','7','8','9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-sequence = [latters, numbers, symbols]
 print("Welcome to the pass word generator. ")
 lett = int(input(" How many Latters do you want in your Password? : \n"))
 symb = int(input("How many Symbols do you want in your Password? : \n"))
---
+++ Day_9/while_loops.py
@@ -0,0 +1,5 @@
+# count from 1 to 5
+i = 1
+while i <= 100:
+    print(i)
+    i += 1
