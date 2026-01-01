1f6d78f967c396d43571adca1b3d27d9bc317c7c
gopaldas11 <gopaldas535251@gmail.com>
2026-01-01 18:12:40 +0600

    2sum problem

---
+++ Leet-code_2sum-problem/the.main-problem.py
@@ -0,0 +1,8 @@
+#This problem was not solved by me , but i promice i will come back  and solve it
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}
+        for i, num in enumerate(nums):
+            if target - num in seen:
+                return [seen[target - num], i]
+            seen[num] = i
